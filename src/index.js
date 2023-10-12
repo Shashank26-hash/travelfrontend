2e9d3998a8625e201b14190077f152eae681c7f6
unknown <shashanksurin2001@gmail.com>
2023-10-12 16:05:35 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'; 
+ 
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
